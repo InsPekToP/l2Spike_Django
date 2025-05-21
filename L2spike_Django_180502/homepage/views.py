c16a27445ed06094fe0099dd016698f53d983779
Offsite computer <cloud_computer@example.com>
2025-05-21 09:27:12 +0300

    добавил шаблоны html base home about news

--- L2spike_Django_180502/homepage/views.py
+++ L2spike_Django_180502/homepage/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def home(request):
+    return HttpResponse('<h3>Главная страница</h3>')
+
+
+def about(request):
+    return HttpResponse('<h3>О сервере </h3>')
+
+
+def news(request):
+    return HttpResponse('<h3>Новости</h3>')
