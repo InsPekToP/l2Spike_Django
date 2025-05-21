4d4a19b799833accac2a9950d8b4194883ba273e
Offsite computer <cloud_computer@example.com>
2025-05-21 19:53:20 +0300

    добавил файлы css

--- L2spike_Django_180502/homepage/views.py
+++ L2spike_Django_180502/homepage/views.py
@@ -1,14 +1,13 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 def home(request):
-    return HttpResponse('<h3>Главная страница</h3>')
+    return render(request,'homepage/home.html')
 
 
 def about(request):
-    return HttpResponse('<h3>О сервере </h3>')
+    return render(request,'homepage/about.html')
 
 
 def news(request):
-    return HttpResponse('<h3>Новости</h3>')
+    return render(request,'homepage/news.html')
