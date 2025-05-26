38e43330ca5da665e7863f8c580467dacaaa4846
Offsite computer <cloud_computer@example.com>
2025-05-26 18:15:45 +0300

    добавил инпут поля для registration.html

--- L2spike_Django_180502/users/views.py
+++ L2spike_Django_180502/users/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
-
+from django.contrib.auth.forms import UserCreationForm
 
 def register(request):
+    form = UserCreationForm()
     context ={
-            'title' : 'Регистрация • L2Spike'
-            }
+        'title' : 'Регистрация • L2Spike',
+        'form' : form
+        }
     return render(request,'users/registration.html', context)
