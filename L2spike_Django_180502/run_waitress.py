4eb3cbc6e55a938fba58af9a53a3a5c4069cb3a5
Offsite computer <cloud_computer@example.com>
2025-07-22 13:52:11 +0300

    подправил run_waitress

--- L2spike_Django_180502/run_waitress.py
+++ L2spike_Django_180502/run_waitress.py
@@ -1,5 +1,7 @@
+import logging
 from waitress import serve
 from L2spike_Django_180502.wsgi import application
 
 if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
     serve(application, host='0.0.0.0', port=8001)
